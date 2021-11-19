cc76238c8257e2554fc1a97e213cfb2408966c1b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-20 00:21:11 +0900

    不要なものを撤去し、ヘルスチェックを追加

---
+++ backend/PurchaseOrderSystem/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PurchaseOrderSystem.Responses;
+
+namespace PurchaseOrderSystem.Controllers
+{
+	/// <summary>
+	/// ヘルスチェック用コントローラ
+	/// </summary>
+	public class HealthCheckController : Controller
+	{
+		[Route("/")]
+		public IActionResult Index()
+		{
+			return Json(new HealthCheckResponse());
+		}
+	}
+}
---
+++ backend/PurchaseOrderSystem/Responses/HealthCheckResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseOrderSystem.Responses
+{
+	/// <summary>
+	/// ヘルスチェック用レスポンス
+	/// </summary>
+	public class HealthCheckResponse
+	{
+		/// <summary>
+		/// メッセージ
+		/// </summary>
+		public string Message { get; private set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public HealthCheckResponse()
+		{
+			Message = "Alive";
+		}
+	}
+}
