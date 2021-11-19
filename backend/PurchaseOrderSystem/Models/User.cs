45aca329ff7eb1b0003ce206e74433c5a637d869
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-20 00:46:55 +0900

    ユーザエンティティとロールエンティティを追加

---
+++ backend/PurchaseOrderSystem/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseOrderSystem.Models
+{
+	/// <summary>
+	/// ユーザエンティティ
+	/// </summary>
+	public class User : IdentityUser
+	{
+	}
+}
