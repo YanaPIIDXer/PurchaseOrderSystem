3eb43fde31d46bdb496f17c4787f93e68fd7c1ed
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-20 00:42:00 +0900

    DBコンテナへの接続設定

---
+++ backend/PurchaseOrderSystem/Context/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurchaseOrderSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseOrderSystem.Context
+{
+	/// <summary>
+	/// データベースコンテキスト
+	/// </summary>
+	public class DatabaseContext : DbContext
+	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="options">オプション</param>
+		public DatabaseContext(DbContextOptions<DatabaseContext> options)
+			: base(options)
+		{
+		}
+
+		// === 各種テーブルへのプロパティ ===
+		public DbSet<User> Users { get; set; }
+		public DbSet<Role> Roles { get; set; }
+	}
+}
--- backend/PurchaseOrderSystem/Startup.cs
+++ backend/PurchaseOrderSystem/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PurchaseOrderSystem.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,13 @@ namespace PurchaseOrderSystem
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+			var password = "development";		// TODO:ä¬ã´ïœêîÇ…ìÀÇ¡çûÇﬁ
+			var connString = string.Format("server={0};user={1};password={2};database=purchase_order_system", "db", "development", password);
+			services.AddDbContext<DatabaseContext>(options =>
+			{
+				options.UseMySql(connString, ServerVersion.AutoDetect(connString));
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
